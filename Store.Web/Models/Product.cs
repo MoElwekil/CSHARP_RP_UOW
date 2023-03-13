18336005f0ac5deda3fec99409862ec819802ab3
Mido.Elwekil <Mido.Elwekil@LDNLT3H3V2M3>
2023-03-13 17:30:13 +0000

    Add project files.

---
+++ Store.Web/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Store.Web.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? ProducedBy { get; set; }
+        public string? Description { get; set; }
+        public string? InsertedBy { get; set; }
+        public DateTime ProducedOn { get; set; }
+    }
+}
